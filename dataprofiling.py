688dd6d1368d2de7f41e3fc66d8bd861e3a95884
Ibrahim Diarra <33433305+diarrai@users.noreply.github.com>
2024-06-10 19:43:17 +0000

    Update dataprofiling.py

--- dataprofiling.py
+++ dataprofiling.py
@@ -1,10 +1,10 @@
-#from nbformat import write
+from nbformat import write
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import ydata_profiling
-#from streamlit_pandas_profiling import st_profile_report
-#from ydata_profiling import ProfileReport
+import ydata_profiling
+from streamlit_pandas_profiling import st_profile_report
+from ydata_profiling import ProfileReport
 
 st.title("MRTC-UCRBO Data Profiling App")
 st.subheader("Cette application vous aidera à faire de l'exploration de données")
